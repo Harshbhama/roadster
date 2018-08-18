05854fca47cd6034399f02dcc4c6fb7949cdb501
HARSH <HARSHBHAMA96@GMAIL.COM>
2018-08-18 19:32:17 +0530

    initial css update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+	protect_from_forgery
+	
+	def after_sign_in_path_for(resource)
+	  posts_path
+	end
+	def after_sign_out_path_for(resource)
+	  posts_path
+	end
+	def after_sign_up_path_for(resource)
+	  posts_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get 'index/home', to: 'index#home'
 
+  root 'index#home'
+
 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
