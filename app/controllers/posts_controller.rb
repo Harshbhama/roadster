fbd12ed66b4afef04b18fcb6ec2d238448d6614b
HARSH <HARSHBHAMA96@GMAIL.COM>
2018-08-18 12:07:44 +0530

    land-up page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,9 @@ class PostsController < ApplicationController
 
   # GET /posts
   # GET /posts.json
+
+
+
   def index
     @posts = Post.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
+
+  get 'index/home', to: 'index#home'
+
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
