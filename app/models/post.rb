637a8b7458f80d894e01ca93f6523d164a3e0257
HARSH <HARSHBHAMA96@GMAIL.COM>
2018-08-20 19:43:51 +0530

    add comments style

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
 
 	belongs_to :user
+	has_many :comments
 
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
          has_many :posts;
+         has_many :comments;
+
 end
