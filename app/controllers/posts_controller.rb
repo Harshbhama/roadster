82de04578a2d07bcee3b5762f6d76eda6fc26260
HARSH <HARSHBHAMA96@GMAIL.COM>
2018-08-31 22:57:01 +0530

    add changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,6 +32,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    
     @post.user_id = current_user.id;
 
     respond_to do |format|
